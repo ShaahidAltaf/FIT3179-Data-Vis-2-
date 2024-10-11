cef2628cecfbb63a6761f9345f6911c3c212e939
ShaahidAltaf <128887366+ShaahidAltaf@users.noreply.github.com>
2024-10-11 15:38:42 +0800

    Add files via upload

--- vegalite_vis.js
+++ vegalite_vis.js
@@ -1,5 +1,9 @@
-var spec2 = "test.vg.json";
+var spec2 = "test.vg.json";  // Path to your first chart spec (map chart)
 vegaEmbed('#map', spec2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
+var spec3 = "stackedChart.vg.json";  // Path to your second chart spec (stacked area chart)
+vegaEmbed('#Stacked_chart', spec3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
